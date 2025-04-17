24ea892383d8e1fc87161dda925901d25f4b02cd
Maksim-Sebelev <7905111@list.ru>
2025-04-17 16:52:10 +0300

    fixed bugs

--- 4/4-2-triangle.py
+++ 4/4-2-triangle.py
@@ -1,12 +1,11 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 import decimal2binary as d2b
-from matplotlib import pyplot as plt
-import numpy as np
+
 
 GPIO.setwarnings(False)
 
-dac = [26, 19, 13, 6, 5, 11, 9, 10]
+dac = []
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(dac, GPIO.OUT)
@@ -21,12 +20,15 @@ try:
     while True:
         GPIO.output(dac, d2b.decimal2binary(x))
 
-        if   x == 0:    inc_flag = 1
-        elif x == 255:  inc_flag = 0
+        if x == 0: 
+            inc_flag = 1
+    
+        elif x == 255:
+            inc_flag = 0
 
         x = x + 1 if inc_flag == 1 else x - 1
 
-        sleep(period/512)
+        time.sleep(period/512)
         t += 1
 
 except ValueError:
